7f59230a34463a257301b45d4e63636d24a880e1
Marcio Moura <murilomoura2005@gmail.com>
2023-10-24 01:43:23 -0300

    Adicionados dados tecnicos

---
+++ js/investidor.js
@@ -0,0 +1,6 @@
+document.querySelector('#botaoInvestir').addEventListener('click', (e) => {
+    
+    e.preventDefault();
+    alert('Você foi adicionado à lista de investidores interessados, fique atento ao seu email, enviaremos em breve um convite para uma reunião com os possíveis investidores!');
+    
+})
